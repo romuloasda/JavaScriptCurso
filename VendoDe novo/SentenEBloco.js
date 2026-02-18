8c74d613c1c104c1e99520177a8e422a19f8103f
Romulo Arantes <romuloasda@hotmail.com>
2026-02-18 14:56:51 -0300

    VS CODE

---
+++ VendoDe novo/SentenEBloco.js
@@ -0,0 +1,7 @@
+console.log("Sentença") // isso é uma sentença
+
+{
+    console.log("Bloco") // isso é um bloco
+}
+
+console.log('')
---
+++ VendoDe novo/dados.js
@@ -0,0 +1,8 @@
+var quantidade = 10
+var preco = 6.4
+var total = quantidade * preco
+
+console.log(total)
+quantidade = 20
+console.log(quantidade)
+console.log(preco)
