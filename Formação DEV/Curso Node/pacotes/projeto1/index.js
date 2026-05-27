efb73c243ad54884e0ff21266dfd4e09e62e4dac
Romulo Arantes <romuloasda@hotmail.com>
2026-05-26 22:43:43 -0300

    npmnode2

--- Formação DEV/Curso Node/pacotes/projeto1/index.js
+++ Formação DEV/Curso Node/pacotes/projeto1/index.js
@@ -1,9 +1,27 @@
 import chalk from "chalk";
+import server from 'server'
+import { DateTime } from 'luxon'
 
-const azul = (texto) => console.log(chalk.blue(texto))
-const aviso = (texto) => console.log(chalk.bgYellow.bold(texto))
-const erro = (texto) => console.log(chalk.bgRed.bold.italic(texto))
+const { get } = server.router
 
-azul('Sou texto')
-aviso('Preciso te avisar de uma coisa..')
-erro('Abortar Missãon')
+
+function imprimeHora(cidade, hora) {
+	console.log(chalk.bgBlue(`Em ${cidade} são ${hora}`))
+}
+
+server({ port: 8080 }, [
+	get('/', (ctx) => "Olá Mundo"),
+	get('/roma', () => {
+		const h = DateTime.local({ zone: 'Europe/Rome' }).toFormat('HH:mm:ss')
+		imprimeHora('roma', h)
+		return h
+	})
+])
+
+// const azul = (texto) => console.log(chalk.blue(texto))
+// const aviso = (texto) => console.log(chalk.bgYellow.bold(texto))
+// const erro = (texto) => console.log(chalk.bgRed.bold.italic(texto))
+
+// azul('Sou texto')
+// aviso('Preciso te avisar de uma coisa..')
+// erro('Abortar Missãon')
